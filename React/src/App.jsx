f504a1ae7d17e793ae599a2b4ebcbbec3813571e
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-11-12 15:19:34 +0200

    feat

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,27 +1,55 @@
 import { useState } from "react";
 import { UserDetail } from "./components/UserDetail";
 
-export default function App(){
-    const [users , setUsers] = useState([
+export default function App() {
+    const [username, setUsername] =useState("");
+    const [email, setEmail] =useState("");
+    const [counter, setCounter] = useState(4)
+
+    const [users, setUsers] = useState([
         {
             id: 1,
             username: 'anson',
-             email: 'anson@gmail.com'
+            email: 'anson@gmail.com'
         },
         {
             id: 2,
             username: 'james',
-             email: 'james@gmail.com'
+            email: 'james@gmail.com'
         },
         {
             id: 3,
             username: 'john',
-             email: 'john@gmail.com'
+            email: 'john@gmail.com'
         }
     ])
 
     return <div>
-        {users.map((users)=>(
+        <div>
+        <form onSubmit={(e)=>{
+            e.preventDefault();
+            const newUser = {
+                id: counter,
+                username,
+                email
+            }
+            setCounter((currentCounter)=> currentCounter + 1);
+            setUsers((currentUserState)=> [...currentUserState, newUser])
+        }}>
+            <div>
+                <label htmlFor="username">Username: </label>
+                <input  value={username} onChange={(e)=> setUsername(e.target.value)} id="username" type="text" />
+                <br />
+            </div>
+            <div>
+                <label htmlFor="email">Email: </label>
+                <input id="email" value={email} onChange={(e)=> setEmail(e.target.value)} type="text" />
+            </div>
+            <button>Add users</button>
+        </form>
+        </div>
+        <br />
+        {users.map((users) => (
             <UserDetail key={users.id} users={users} setUsers={setUsers} />
         ))}
     </div>
