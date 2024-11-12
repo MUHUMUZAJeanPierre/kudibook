312fe35a5defec7f61e2e13e7d60b2fb347b3391
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-11-12 16:42:25 +0200

    feat/fetchingTwodifferentMethod_abortController

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -8,19 +8,42 @@ export default function App(){
         console.log("rendering...");
         document.title = "React tutorial" + counter;
     }, [sync])
-    // useEffect(() => {
-        // This code runs every time the component re-renders
-    //   });
-      
-    // useEffect(() => {
-        // This code runs only once after the initial render
-    //   }, []);
 
-    // useEffect(() => {
-    //     // This code runs only when dep1 or dep2 change
-    //   }, [dep1, dep2]);
+    // useEffect(()=>{
+    //      fetch(`https://jsonplaceholder.typicode.com/users`,  {
+    //         method: 'GET',
+    //     }).then((response)=>{
+    //         return response.json()
+            
+    //     }).then((data)=>{
+    //         console.log(data)
+    //     })
+    //     .catch((error)=>{
+    //         console.error("Error fetching users: ", error);
+    //     })
+    // })
       
-      
+    useEffect(() => {
+        const controller = new AbortController();
+
+        async function fetUsers() {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/users',{
+                    signal: controller.signal
+                });
+                const json = await response.json();
+                console.log("Fetched users:", json);  
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        }
+        
+        fetUsers() ; 
+        return ()=>{
+            controller.abort();
+        }
+    }, []);  
+    
     return <div>
         <div>You clicked the button {counter} times </div>
         <button onClick={()=>setCounter((count)=> count +1)}>Click Me</button>
